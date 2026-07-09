bba53326e87e69aadec6c4a4724d202e3a985fb0
sasa-gamer47 <giacomofilippi237@gmail.com>
2026-07-09 11:30:53 +0200

    typescript fix

--- my-project/components/SongViewer.tsx
+++ my-project/components/SongViewer.tsx
@@ -199,7 +199,7 @@ export default function SongViewer({
       <div className="col-start-1 col-end-2 row-start-2 row-end-3 overflow-hidden h-full w-full">
         <SongLyricsArea
           scrollContainerRef={scrollContainerRef}
-          content={song.content || undefined}
+          content={(song.content as any) || []} // <-- Parentesi + "as any" + array vuoto
           transpose={transpose}
           simplify={simplify}
           fontSize={settings.default_font_size}
